3de2d236bc6ecfb21bb3af120a03608da040b2de
MansourBalkhawmi <100216327+MansourBalkhawmi@users.noreply.github.com>
2024-08-20 15:33:31 +0000

    service

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config
 
-const port = process.env.PORT ?? 3003;
+const port = process.env.PORT || 4000;
 app.server.listen(port,() => { 
     console.log(`Server is running on port ${port}`); 
 });
